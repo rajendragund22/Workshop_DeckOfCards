6256136f21ed317052b21bbc69f871be64d330b2
RajendraGund <rajendragund2015@gmail.com>
2021-09-13 09:16:51 +0530

    Added : Program for the Create 52 unique cards

--- src/main/java/com/bridgelabz/DecksOfCards.java
+++ src/main/java/com/bridgelabz/DecksOfCards.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 public class DecksOfCards {
     public static void main(String[] args) {
         System.out.println("Welcome into the Decks of Cards!!! Thank you");
-
+        CardsPlay cardsPlay = new CardsPlay();
+        cardsPlay.generateUniqueCards();
     }
 }
