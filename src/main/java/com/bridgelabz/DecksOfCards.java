16dc180562ef843742fbee91dc124f721101af54
RajendraGund <rajendragund2015@gmail.com>
2021-09-13 08:53:53 +0530

    Added : Welcome message for the User

---
+++ src/main/java/com/bridgelabz/DecksOfCards.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class DecksOfCards {
+    public static void main(String[] args) {
+        System.out.println("Welcome into the Decks of Cards!!! Thank you");
+
+    }
+}
